798464c5bb1ffba801970327effc2bccb45029d0
Jhan Carlos <148498551+AdrianGarciaVidal@users.noreply.github.com>
2024-04-20 16:02:46 -0500

    Configuracion de rutas

--- eventos-deportivos/routes/web.php
+++ eventos-deportivos/routes/web.php
@@ -2,11 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\EventosController;
+use App\Http\Controllers\ParticipantesController;
+use App\Http\Controllers\EquiposController;
+use App\Http\Controllers\HomeController;
 
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/participantes', [ParticipantesController::class, 'index'])->name('participantes');
+Route::get('/participantes/editar', [ParticipantesController::class, 'edit'])->name('participantes.editar');
+
+Route::get('/eventos', [EventosController::class, 'index'])->name('eventos');
+Route::get('/eventos/editar', [EventosController::class, 'edit'])->name('eventos.editar');
+
+Route::get('/equipos', [EquiposController::class, 'index'])->name('equipos');
+Route::get('/equipos/editar', [EquiposController::class, 'edit'])->name('equipos.editar');
